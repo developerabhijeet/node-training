c3b736e7d050aa97e2a733a76ef68d734448e51c
Shaily-sharma <shisharma@bestpeers.com>
2023-09-20 12:48:52 +0530

    create-server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const port = 3000
+const publicPath = path.join(__dirname , "/public")
+app.use(express.static(publicPath))
+app.get("/" , (req, res) => {
+  res.sendFile(`${publicPath}/index.html`)
+})
+app.listen(port , () => {
+  console.log(`Localhost is running in port : ${port}`);
+})
