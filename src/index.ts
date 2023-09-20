2234ff72ae8c6e2fb0c083bd9bb468f2dc7ab5b6
lokeshpatel <lopatel@bestpeers.com>
2023-09-20 16:19:20 +0530

    intial setup

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { request } from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import http from "http";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.get('/',(req:any , res:any)=>{
+    res.send({message:"hello"})
+}
+)
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+
+server.listen(4500, () => {
+    console.log("server is running on port 4500")
+})
