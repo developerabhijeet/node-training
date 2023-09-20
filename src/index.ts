94b1f6ecd608bd0a68ba087e6094830d4f16609f
chandraprakashk937 <chandraprakashk9589@gmail.com>
2023-09-20 11:12:20 +0530

    create-server

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import http from "http";
-import compression from "compression";
-import cors from "cors";
+// const express =require('express')
+// import cookieParser from "cookie-parser";
+// import bodyParser from "body-parser";
+// import http from "http";
+// import compression from "compression";
+// import cors from "cors";
 
-const app = express();
+// const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//   })
+// );
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// app.use(compression());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 
-server.listen(4500, () => {
-    console.log("server is running on port 4500")
+// server.listen(3000, () => {
+//     console.log("server is running on port 3000")
+// })
+
+let express = require('express');
+let app = express();
+app.get('/',function(req:any,res:any){
+   res.send('hello nodejs')
 })
+
+app.listen(3000);
+
