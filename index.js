a644cd32fb1505ffd14c4d96c1fc02fbbff6e1a0
aakash <punasiya.aakash@gmail.com>
2023-09-23 22:55:12 +0530

    add file and using this in the routes

--- index.js
+++ index.js
@@ -11,17 +11,21 @@
 //   console.log(`server start at port ${port}`);
 // });
 
-//Create server with express 
+//Create server with express using File as well
 
 const express = require('express')
 const baseUrl =  "http://localhost:"
+const path = require("path")
 const app = express()
-const port = 3000;
+const port = 3001;
 app.get("/",(req,res) => {
     res.send("Hello express call here")
 })
-app.get("/newurl",(req,res)=>{
-    res.send("new url call here")
+app.get("/contact",(req,res)=>{
+    res.send("<h1>Contact page using direct HTML Tag in the response...</h1>")
+})
+app.get("/service",(req,res)=>{
+    res.sendFile(path.join(__dirname + "/service.html"))
 })
 app.listen(port,()=>{
     console.log(`Server is runnin on this Port number ${baseUrl}${port}`);
