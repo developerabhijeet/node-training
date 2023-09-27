e1e0d8b331cdee290768af595febcdedac5a106f
pushkar <puwagh@bestpeers.com>
2023-09-27 10:55:17 +0530

    js to ts setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+
+import express from "express";
+const PORT = 3001;
+
+const app: express.Express = express();
+
+app.use(express.json());
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Get Started! :)");
+});
+
+app.get('/test', (req: express.Request, res: express.Response) =>{
+    res.sendFile(__dirname + "/test.html")
+});
+
+app.get("/user/:name", (req: express.Request, res: express.Response) => {
+  res.json(`Hi ${req.params.name }`);
+});
+
+app.listen(PORT, () => 
+    console.log(`app is running on this port ${PORT}`)
+);
